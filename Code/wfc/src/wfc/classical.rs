e4effd932194d28d68047e5f4b764d03aa830447
Floris <mgfloris0106@gmail.com>
2026-01-20 13:14:57 +0100

    Failed attempt to add qiskit-rs

--- Code/wfc/src/wfc/classical.rs
+++ Code/wfc/src/wfc/classical.rs
@@ -9,7 +9,7 @@ pub fn generate(size: (usize, usize)) -> Result<Vec<Tile>> {
     let mut wavefunction: Vec<u8> = vec![0xf; size.0 * size.1];
 
     loop {
-        if !collapse_min(&mut wavefunction, size) {
+        if !collapse(&mut wavefunction, size) {
             break;
         }
 
@@ -30,7 +30,6 @@ pub fn generate(size: (usize, usize)) -> Result<Vec<Tile>> {
         .collect::<Result<Vec<_>>>()
 }
 
-#[allow(dead_code)]
 fn collapse(wavefunction: &mut [u8], size: (usize, usize)) -> bool {
     let mut count = 0;
     let mut collapse_pos = (usize::MAX, usize::MAX);
@@ -53,43 +52,6 @@ fn collapse(wavefunction: &mut [u8], size: (usize, usize)) -> bool {
     let index = get_index(collapse_pos, size);
     let mut options = wavefunction[index];
     let chosen_option = rand::random_range(0..options.count_ones());
-    for _ in 0..chosen_option {
-        options &= options - 1;
-    }
-    options &= !(options - 1);
-    wavefunction[index] = options;
-
-    true
-}
-
-#[allow(dead_code)]
-fn collapse_min(wavefunction: &mut [u8], size: (usize, usize)) -> bool {
-    let mut min_options = 5;
-    let mut count = 0;
-    let mut collapse_pos = (usize::MAX, usize::MAX);
-    for y in 0..size.1 {
-        for x in 0..size.0 {
-            let options = wavefunction[get_index((x, y), size)].count_ones();
-            if (2..min_options).contains(&options) {
-                min_options = options;
-                count = 1;
-                collapse_pos = (x, y);
-            } else if options == min_options {
-                count += 1;
-                if rand::random_range(0..count) < 1 {
-                    collapse_pos = (x, y);
-                }
-            }
-        }
-    }
-
-    if collapse_pos == (usize::MAX, usize::MAX) {
-        return false;
-    }
-
-    let index = get_index(collapse_pos, size);
-    let mut options = wavefunction[index];
-    let chosen_option = rand::random_range(0..min_options);
     for _ in 0..chosen_option {
         options &= options - 1;
     }
--- Code/wfc/src/wfc/mod.rs
+++ Code/wfc/src/wfc/mod.rs
@@ -1,4 +1,5 @@
 pub mod classical;
+pub mod quantum;
 
 pub enum Tile {
     Water,
